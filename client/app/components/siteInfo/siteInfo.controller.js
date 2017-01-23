2a49e203bb10530996f36eaf75a25840be80531b
Michael Frohberg <michaelfrohberg@Michaels-MacBook-Air.local>
2017-01-23 16:25:22 -0500

    add logic for icons

--- client/app/components/siteInfo/siteInfo.controller.js
+++ client/app/components/siteInfo/siteInfo.controller.js
@@ -4,9 +4,16 @@ class SiteInfoController {
   constructor () {
     this.name = 'siteInfo'
     this.accounts = accounts
+    this.iconsActive = false
+  }
+  setIconsActive () {
+    this.iconsActive = true
+    console.log(this.iconsActive)
+  }
+  setIconsInactive () {
+    this.iconsActive = false
+    console.log(this.iconsActive)
   }
 }
-
-console.log(accounts)
 
 export default SiteInfoController
